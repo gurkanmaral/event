9ed97685ce29d20ee5b77d4ae04bbffbbab5fbcb
gurkanmaral <gurkanmaral1@gmail.com>
2023-12-16 22:23:14 +0300

    forth commit

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -32,6 +32,6 @@ const EventSchema = new Schema({
 
 })
 
-const Event = models.Event || model('UseEventr',EventSchema);
+const Event = models.Event || model('Event',EventSchema);
 
 export default Event;
